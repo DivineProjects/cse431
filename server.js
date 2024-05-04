b3bf23f51db855d63a94616af82cc4a4e52fa391
divineProjects <divinejigu@gmail.com>
2024-05-04 10:16:06 +0200

    week 1: mango database installation and config

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express"); // import express
 const app = express(); // launch app instance of express
+const mongodb = require("./data/database.js"); // import database
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => {console.log(`Running on port ${port}`)})
+mongodb.initDb((err) =>{
+    if (err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, () => {console.log(`Database listening and node is Running on port ${port}`)});
+    }
+});
 
 
